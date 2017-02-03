c227e3997d0ab608f7f2b8bc2971c569ac7bc610
Isaiah Cruz <icruz@stratpoint.com>
2017-02-03 11:24:26 +0800

    Added config file

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const config = require('../config');
+
 const app = express();
 
 module.exports = app;
---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {
+  dev: {
+  },
+  prod: {
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = config.prod;
+} else {
+  module.exports = config.dev;
+}
