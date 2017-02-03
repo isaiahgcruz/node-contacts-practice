79813ca2910b82f5e2406428fc5b55a3002f2729
Isaiah Cruz <icruz@stratpoint.com>
2017-02-03 11:18:59 +0800

    Initialized server

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const app = require('./app');
+
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('an error occured', err);
+  }
+  console.log(`Server is listening on ${port}`);
+});
